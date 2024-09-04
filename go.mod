6c4436b31dac6b339c6b7481de25eb420726db11
Ultra Derek <ultradereks@gmail.com>
2024-09-04 12:24:39 -0400

    I'm fcking confused

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/ultraderek/sttserver
 
 go 1.23.0
 
-require github.com/gen2brain/malgo v0.11.22
+require (
+	github.com/gen2brain/malgo v0.11.22
+	github.com/hajimehoshi/go-mp3 v0.3.4
+	github.com/youpy/go-wav v0.3.2
+)
+
+require (
+	github.com/youpy/go-riff v0.1.0 // indirect
+	github.com/zaf/g711 v0.0.0-20190814101024-76a4a538f52b // indirect
+)
---
+++ mic/mic.go
@@ -0,0 +1,200 @@
+package mic
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/hajimehoshi/go-mp3"
+	"github.com/youpy/go-wav"
+
+	"github.com/gen2brain/malgo"
+)
+
+func Example0() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("No input file.")
+		os.Exit(1)
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	var reader io.Reader
+	var channels, sampleRate uint32
+
+	switch strings.ToLower(filepath.Ext(os.Args[1])) {
+	case ".wav":
+		w := wav.NewReader(file)
+		f, err := w.Format()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		reader = w
+		channels = uint32(f.NumChannels)
+		sampleRate = f.SampleRate
+
+	case ".mp3":
+		m, err := mp3.NewDecoder(file)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		reader = m
+		channels = 2
+		sampleRate = uint32(m.SampleRate())
+	default:
+		fmt.Println("Not a valid file.")
+		os.Exit(1)
+	}
+
+	ctx, err := malgo.InitContext(nil, malgo.ContextConfig{}, func(message string) {
+		fmt.Printf("LOG <%v>\n", message)
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer func() {
+		_ = ctx.Uninit()
+		ctx.Free()
+	}()
+
+	deviceConfig := malgo.DefaultDeviceConfig(malgo.Playback)
+	deviceConfig.Playback.Format = malgo.FormatS16
+	deviceConfig.Playback.Channels = channels
+	deviceConfig.SampleRate = sampleRate
+	deviceConfig.Alsa.NoMMap = 1
+
+	// This is the function that's used for sending more data to the device for playback.
+	onSamples := func(pOutputSample, pInputSamples []byte, framecount uint32) {
+		io.ReadFull(reader, pOutputSample)
+	}
+
+	deviceCallbacks := malgo.DeviceCallbacks{
+		Data: onSamples,
+	}
+	device, err := malgo.InitDevice(ctx.Context, deviceConfig, deviceCallbacks)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer device.Uninit()
+
+	err = device.Start()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Press Enter to quit...")
+	fmt.Scanln()
+}
+
+func Example1() {
+	ctx, err := malgo.InitContext(nil, malgo.ContextConfig{}, func(message string) {
+		fmt.Printf("LOG <%v>\n", message)
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer func() {
+		_ = ctx.Uninit()
+		ctx.Free()
+	}()
+
+	deviceConfig := malgo.DefaultDeviceConfig(malgo.Duplex)
+	deviceConfig.Capture.Format = malgo.FormatS16
+	deviceConfig.Capture.Channels = 1
+	deviceConfig.Playback.Format = malgo.FormatS16
+	deviceConfig.Playback.Channels = 1
+	deviceConfig.SampleRate = 44100
+	deviceConfig.Alsa.NoMMap = 1
+
+	var playbackSampleCount uint32
+	var capturedSampleCount uint32
+	pCapturedSamples := make([]byte, 0)
+
+	sizeInBytes := uint32(malgo.SampleSizeInBytes(deviceConfig.Capture.Format))
+	onRecvFrames := func(pSample2, pSample []byte, framecount uint32) {
+
+		sampleCount := framecount * deviceConfig.Capture.Channels * sizeInBytes
+
+		newCapturedSampleCount := capturedSampleCount + sampleCount
+
+		pCapturedSamples = append(pCapturedSamples, pSample...)
+
+		capturedSampleCount = newCapturedSampleCount
+
+	}
+
+	fmt.Println("Recording...")
+	captureCallbacks := malgo.DeviceCallbacks{
+		Data: onRecvFrames,
+	}
+	device, err := malgo.InitDevice(ctx.Context, deviceConfig, captureCallbacks)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = device.Start()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Press Enter to stop recording...")
+	fmt.Scanln()
+
+	device.Uninit()
+
+	onSendFrames := func(pSample, nil []byte, framecount uint32) {
+		samplesToRead := framecount * deviceConfig.Playback.Channels * sizeInBytes
+		if samplesToRead > capturedSampleCount-playbackSampleCount {
+			samplesToRead = capturedSampleCount - playbackSampleCount
+		}
+
+		copy(pSample, pCapturedSamples[playbackSampleCount:playbackSampleCount+samplesToRead])
+
+		playbackSampleCount += samplesToRead
+
+		if playbackSampleCount == uint32(len(pCapturedSamples)) {
+			playbackSampleCount = 0
+		}
+	}
+
+	fmt.Println("Playing...")
+	playbackCallbacks := malgo.DeviceCallbacks{
+		Data: onSendFrames,
+	}
+
+	device, err = malgo.InitDevice(ctx.Context, deviceConfig, playbackCallbacks)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = device.Start()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Press Enter to quit...")
+	fmt.Scanln()
+
+	device.Uninit()
+}
