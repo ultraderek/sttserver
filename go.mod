1ae2a3ae6d98843b3007328c751dfaa8ace0e85f
Ultra Derek <ultradereks@gmail.com>
2024-09-19 12:02:58 -0400

    added more packages

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/amitybell/piper v0.0.0-20240130090909-b170e42b1e09
 	github.com/amitybell/piper-voice-jenny v0.0.0-20231118093224-dcf0d49e46b7
 	github.com/gen2brain/malgo v0.11.22
+	github.com/gopxl/beep v1.4.1
 )
 
 require (
@@ -13,6 +14,9 @@ require (
 	github.com/amitybell/piper-asset v0.0.0-20231030194325-d36a29e3b1fd // indirect
 	github.com/amitybell/piper-bin-linux v0.0.0-20231118093037-92b3de178ad8 // indirect
 	github.com/amitybell/piper-bin-windows v0.0.0-20231118093113-cc2cef2f6b74 // indirect
+	github.com/ebitengine/oto/v3 v3.1.0 // indirect
+	github.com/ebitengine/purego v0.7.1 // indirect
 	github.com/klauspost/compress v1.17.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 )
--- voice/voice.go
+++ voice/voice.go
@@ -1,8 +1,14 @@
 package voice
 
 import (
+	"bytes"
+	"time"
+
 	"github.com/amitybell/piper"
 	jenny "github.com/amitybell/piper-voice-jenny"
+	"github.com/gopxl/beep"         // Import the beep package for playing audio
+	"github.com/gopxl/beep/speaker" // Import the speaker package to output the audio
+	"github.com/gopxl/beep/wav"     // Import the wav package to decode the wav audio
 )
 
 func Example() {
@@ -10,9 +16,30 @@ func Example() {
 	if err != nil {
 		panic(err)
 	}
-	wav, err := tts.Synthesize("hello world")
+	wavBytes, err := tts.Synthesize("A rainbow is a meteorological phenomenon that is caused by reflection, refraction and dispersion of light in water droplets resulting in a spectrum of light appearing in the sky.")
+
 	if err != nil {
 		panic(err)
 	}
-	_ = wav
+
+	r := bytes.NewReader(wavBytes)
+
+	streamer, format, err := wav.Decode(r)
+	if err != nil {
+		panic(err) // If there's an error, stop the program
+	}
+
+	// Initialize the speaker with the sample rate from the WAV data
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	// Create a done channel to signal when the audio has finished playing
+	done := make(chan bool)
+
+	// Play the audio and signal the done channel when finished
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true // Signal the done channel
+	})))
+
+	// Wait for the audio to finish playing before allowing the program to exit
+	<-done
 }
